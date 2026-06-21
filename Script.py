1b1bbda597c1ed97e70060a1113b10341792d4d5
userxspy <userxspy@gmail.com>
2026-06-21 13:36:42 +0530

    Translate bot messages to Hindi
    Updated text messages to Hindi for better localization.

--- Script.py
+++ Script.py
@@ -1,150 +1,41 @@
 class script(object):
 
-    START_TXT = """<b>Hey {}, <i>{}</i>
-I Am PowerFul Auto Filter With Link Shortener. Just Add Me To Your Group And Enjoy... ♻️</b>"""
+    START_TXT = """<b>हे {}, <i>{}</i>
 
-    MY_ABOUT_TXT = """👰 Name : <a href=https://t.me/UncutFlixBot>UncutFlix</a>
- 🦹 Creator : <a href='https://t.me/DesiXCloud'>DesiXCloud</a> 
- 🤖 Version : 5.0</b>"""
+मैं तुम्हारा पर्सनल ऑटो फ़िल्टर बॉट हूँ। मूवी या फ़ाइल का नाम भेजो और तुरंत डायरेक्ट लिंक पाओ... ⚡️</b>"""
 
-    MY_OWNER_TXT = """★ Name: Amit Sharma
-★ Username: @RjdTv
-★ Country: Hindustan 🚩"""
+    STATUS_TXT = """🗃️ <b>डेटाबेस स्थिति (Bot Stats):</b>
 
-    STATUS_TXT = """🗃️ Total Files: <code>{}</code>
-🦹‍♂ Total Users: <code>{}</code>
-💬 Total Chats: <code>{}</code>
-🚀 Premium Users: <code>{}</code>
-📂 Used Storage: <code>{}</code>
-🗂 Free Storage: <code>{}</code>
-⏰ Uptime: <code>{}</code>"""
+📂 कुल फ़ाइलें: <code>{}</code>
+🦹 कुल एडमिंस: <code>{}</code>
+🚀 इस्तेमाल की गई स्टोरेज: <code>{}</code>
+🗂️ फ्री स्टोरेज: <code>{}</code>
+⏰ अपटाइम: <code>{}</code>"""
 
-    NEW_GROUP_TXT = """#NewGroup
-Title - {}
-ID - <code>{}</code>
-Username - {}
-Total - <code>{}</code>"""
+    NOT_FILE_TXT = """👋 <b>हे {},
 
-    NEW_USER_TXT = """#NewUser
-★ Name: {}
-★ ID: <code>{}</code>"""
+दिए गए कीवर्ड <code>{}</code> के नाम से डेटाबेस में कोई फ़ाइल नहीं मिली! 🥲
 
-    NOT_FILE_TXT = """👋 Hello {},
+👉 कृपया स्पेलिंग जांचें या सही नाम लिखकर दोबारा सर्च करें।</b>"""
 
-I can't find the <b>{}</b> in my database! 🥲
+    FILE_CAPTION = """<b>📂 {file_name}</b>\n\n<b>♻️ साइज: {file_size}</b>"""
 
-👉 Google Search and check your spelling is correct.
-👉 Please read the Instructions to get better results.
-👉 Or not been released yet."""
-    
-    EARN_TXT = """<b>ʜᴏᴡ ᴛᴏ ᴇᴀʀɴ ꜰʀᴏᴍ ᴛʜɪs ʙᴏᴛ
+    HELP_TXT = """<b>नोट - कमांड्स की सही जानकारी के लिए नीचे दिए गए बटन पर क्लिक करें। ⚙️</b>"""
 
-➥ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴀʟsᴏ ᴇᴀʀɴ ᴍᴏɴᴇʏ ʙʏ ᴜsɪɴɢ ᴛʜɪꜱ ʙᴏᴛ.
+    ADMIN_COMMAND_TXT = """<b>🤖 एडमिन कमांड्स की लिस्ट (Admin Commands):</b>
 
-» sᴛᴇᴘ 1:- ғɪʀsᴛ ʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴀᴅᴅ ᴛʜɪs ʙᴏᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ᴀᴅᴍɪɴ ᴘᴇʀᴍɪssɪᴏɴ.
+🔹 /index_channels - इंडेक्स किए गए चैनल्स चेक करें
+🔹 /stats - बॉट और डेटाबेस का लाइव स्टेटस देखें
+🔹 /delete - क्वेरी का उपयोग करके फ़ाइलें डिलीट करें
+🔹 /delete_all - डेटाबेस की सभी इंडेक्स फाइलें डिलीट करें
+🔹 /restart - बॉट को रीस्टार्ट करें
+🔹 /set_pm_search - पीएम सर्च ऑन/off करें (on/off)
+🔹 /set_fsub - फोर्स सब चैनल सेट करें
+🔹 /remove_fsub - फोर्स सब चैनल हटाएँ"""
 
-» sᴛᴇᴘ 2:- ᴍᴀᴋᴇ ᴀᴄᴄᴏᴜɴᴛ ᴏɴ <a href=https://telegram.me/how_to_download_channel/14>mdisklink.link</a> [ ʏᴏᴜ ᴄᴀɴ ᴀʟsᴏ ᴜsᴇ ᴏᴛʜᴇʀ sʜᴏʀᴛɴᴇʀ ᴡᴇʙsɪᴛᴇ ]
+    USER_COMMAND_TXT = """<b>🛠️ कॉन्फ़िगरेशन कमांड्स (Configuration Commands):</b>
 
-» sᴛᴇᴘ 3:- ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴇʟᴏᴡ ɢɪᴠᴇɴ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ʏᴏᴜʀ sʜᴏʀᴛɴᴇʀ ᴡɪᴛʜ ᴛʜɪs ʙᴏᴛ.
-
-➥ ᴛʜɪꜱ ʙᴏᴛ ɪs ꜰʀᴇᴇ ꜰᴏʀ ᴀʟʟ, ʏᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴛʜɪꜱ ʙᴏᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘs ғᴏʀ ꜰʀᴇᴇ ᴏꜰ ᴄᴏꜱᴛ.</b>"""
-
-    HOW_TXT = """<b>ʜᴏᴡ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ʏᴏᴜʀ ᴏᴡɴ sʜᴏʀᴛɴᴇʀ ‼️
-
-➥ ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ʏᴏᴜʀ ᴏᴡɴ sʜᴏʀᴛɴᴇʀ ᴛʜᴇɴ ᴊᴜsᴛ sᴇɴᴅ ᴛʜᴇ ɢɪᴠᴇɴ ᴅᴇᴛᴀɪʟs ɪɴ ᴄᴏʀʀᴇᴄᴛ ꜰᴏʀᴍᴀᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ
-
-➥ ғᴏʀᴍᴀᴛ ↓↓↓
-
-<code>/set_shortlink sʜᴏʀᴛɴᴇʀ sɪᴛᴇ sʜᴏʀᴛɴᴇʀ ᴀᴘɪ</code>
-
-➥ ᴇxᴀᴍᴘʟᴇ ↓↓↓
-
-<code>/set_shortlink mdisklink.link 5843c3cc645f5077b2200a2c77e0344879880b3e</code>
-
-➥ ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴄʜᴇᴄᴋ ᴡʜɪᴄʜ sʜᴏʀᴛᴇɴᴇʀ ʏᴏᴜ ʜᴀᴠᴇ ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛʜᴇɴ sᴇɴᴅ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ᴛʜᴇ ɢʀᴏᴜᴘ /get_shortlink
-
-📝 ɴᴏᴛᴇ:- ʏᴏᴜ sʜᴏᴜʟᴅ ɴᴏᴛ ʙᴇ ᴀɴ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ ɪɴ ɢʀᴏᴜᴘ. sᴇɴᴅ ᴄᴏᴍᴍᴀɴᴅ ᴡɪᴛʜᴏᴜᴛ ʙᴇɪɴɢ ᴀɴ ᴀɴᴏɴʏᴍᴜs ᴀᴅᴍɪɴ.</b>"""
-
-    IMDB_TEMPLATE = """✅ I Found: <code>{query}</code>
-
-🏷 Title: <a href={url}>{title}</a>
-🎭 Genres: {genres}
-📆 Year: <a href={url}/releaseinfo>{year}</a>
-🌟 Rating: <a href={url}/ratings>{rating} / 10</a>
-☀️ Languages: {languages}
-📀 RunTime: {runtime} Minutes
-
-🗣 Requested by: {message.from_user.mention}
-©️ Powered by: <b>{message.chat.title}</b>"""
-
-    FILE_CAPTION = """<b>📂 {file_name}</b>
-<b>♻️ Size: {file_size}</b>
-<b>⚡ Powered By:- @DesiXCloud</b>"""
-
-    WELCOME_TEXT = """👋 Hello {mention}, Welcome to {title} group! 💞"""
-
-    HELP_TXT = """<b>Note - <spoiler>Try each command without any argument to see more details 😹</spoiler></b>"""
-    
-    ADMIN_COMMAND_TXT = """<b>Here is bot admin commands 👇
-
-/index_channels - to check how many index channel id added
-/stats - to get bot status
-/delete - to delete files using query
-/delete_all - to delete all indexed file
-/broadcast - to send message to all bot users
-/grp_broadcast - to send message to all groups
-/pin_broadcast - to send message as pin to all bot users.
-/pin_grp_broadcast - to send message as pin to all groups.
-/restart - to restart bot
-/leave - to leave your bot from particular group
-/unban_grp - to enable group
-/ban_grp - to disable group
-/ban_user - to ban a users from bot
-/unban_user - to unban a users from bot
-/users - to get all users details
-/chats - to get all groups
-/invite_link - to generate invite link
-/set_pm_search - to do pm search on/off
-/index - to index bot accessible channels</b>"""
-    
-    USER_COMMAND_TXT = """<b>Here is bot user commands 👇
-
-/start - to check bot alive or not
-/settings - to change group settings as your wish
-/set_template - to set custom imdb template
-/set_caption - to set custom bot files caption
-/set_shortlink - group admin can set custom shortlink
-/get_custom_settings - to get your group settings details
-/set_welcome - to set custom new joined users welcome message for group
-/set_tutorial - to set custom tutorial link in result page button
-/id - to check group or channel id
-/set_fsub - to set force subscribe channels
-/remove_fsub - to remove all force subscribe channel</b>"""
-
-    PREMIUM_PLAN_TEXT = """<b><i><u>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟᴀɴs - </u>
-
-- 30ʀs - 1 ᴡᴇᴇᴋ
-- 50ʀs - 1 ᴍᴏɴᴛʜs
-- 120ʀs - 3 ᴍᴏɴᴛʜs
-- 220ʀs - 6 ᴍᴏɴᴛʜs
-
-<u>🎁 ᴘʀᴇᴍɪᴜᴍ ғᴇᴀᴛᴜʀᴇs 🎁</u>
-
-○ ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴠᴇʀɪғʏ
-○ ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴏᴘᴇɴ ʟɪɴᴋ
-○ ᴅɪʀᴇᴄᴛ ғɪʟᴇs   
-○ ᴀᴅ-ғʀᴇᴇ ᴇxᴘᴇʀɪᴇɴᴄᴇ 
-○ ʜɪɢʜ-sᴘᴇᴇᴅ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ                           
-○ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴏᴠɪᴇs & sᴇʀɪᴇs                                                                         
-○ ꜰᴜʟʟ ᴀᴅᴍɪɴ sᴜᴘᴘᴏʀᴛ                              
-○ ʀᴇǫᴜᴇsᴛ ᴡɪʟʟ ʙᴇ ᴄᴏᴍᴘʟᴇᴛᴇᴅ ɪɴ 1ʜ ɪꜰ ᴀᴠᴀɪʟᴀʙʟᴇ   
-
-✨ ᴜᴘɪ ɪᴅ - <code>{}</code>
-
-ᴄʟɪᴄᴋ ᴛᴏ ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴀᴄᴛɪᴠᴇ ᴘʟᴀɴ /myplan
-
-💢 ᴍᴜsᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛ ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ
-
-‼️ ᴀғᴛᴇʀ sᴇɴᴅɪɴɢ ᴀ sᴄʀᴇᴇɴsʜᴏᴛ ᴘʟᴇᴀsᴇ ɢɪᴠᴇ ᴜs sᴏᴍᴇ ᴛɪᴍᴇ ᴛᴏ ᴀᴅᴅ ʏᴏᴜ ɪɴ ᴛʜᴇ ᴘʀᴇᴍɪᴜᴍ ʟɪsᴛ</i></b>"""
-
-
+🔹 /start - बॉट का लाइव स्टेटस चेक करें
+🔹 /set_caption - कस्टम फ़ाइल कैप्शन सेट करें
+🔹 /id - अपनी यूज़र आईडी या रिप्लाई किए गए मैसेज की आईडी देखें
+🔹 /catbox - किसी फोटो/वीडियो को कैटबॉक्स लिंक में बदलें"""
