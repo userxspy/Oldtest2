9973608112205369b4fcc79e012b13e9ea9ed705
userxspy <userxspy@gmail.com>
2026-06-21 13:53:54 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,20 +1,18 @@
-from pyrogram import Client, filters
+from hydrogram import Client, filters
 from info import INDEX_CHANNELS
 from database.ia_filterdb import save_file
 
-media_filter = filters.document | filters.video | filters.audio
+# सिर्फ डॉक्यूमेंट, वीडियो और ऑडियो फाइल्स को ही कैप्चर करेंगे
+MEDIA_FILTER = filters.document | filters.video | filters.audio
 
-
-@Client.on_message(filters.chat(INDEX_CHANNELS) & media_filter)
-async def media(bot, message):
-    """Media Handler"""
-    for file_type in ("document", "video", "audio"):
-        media = getattr(message, file_type, None)
-        if media is not None:
-            break
-    else:
+@Client.on_message(filters.chat(INDEX_CHANNELS) & MEDIA_FILTER & filters.incoming)
+async def auto_channel_indexer(bot, message):
+    """इंडेक्स चैनल्स में आने वाली नई फाइल्स को ऑटोमैटिकली डेटाबेस में लाइव सेव करने का इंजन"""
+    # मैसेज में से असली मीडिया टाइप को ढूंढें
+    media = message.document or message.video or message.audio
+    if not media:
         return
 
-    media.file_type = file_type
-    media.caption = message.caption
+    # कैप्शन अटैच करें और बिना किसी वैलीडेशन ओवरहेड के सीधे मोटर ड्राइवर से सेव करें
+    media.caption = message.caption or ""
     await save_file(media)
